64a32d319ac96d679875bc250977333cf5ee7698
Rose Francis <rosennabs@gmail.com>
2024-03-31 16:31:36 -0600

    Define reducer function

--- src/redux/users/usersReducer.js
+++ src/redux/users/usersReducer.js
@@ -10,3 +10,32 @@ const initialState = {
   users: [],
   error: ''
 }
+
+
+const usersReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_USERS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+      };
+
+    case FETCH_USERS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        users: action.payload,
+        error: "",
+      };
+
+    case FETCH_USERS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        users: [],
+        error: action.payload,
+      };
+  }
+};
+
+export default usersReducer;
