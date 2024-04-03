e19644653a221ac384766e3c982f5adcbaae7ca0
Rose Francis <rosennabs@gmail.com>
2024-04-02 22:11:03 -0600

    Apply more styling to component

--- src/components/RenderUsers.jsx
+++ src/components/RenderUsers.jsx
@@ -1,17 +1,19 @@
-import React from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { fetchUsers, handleCloseRequest } from '../redux/users/usersActions'
-import fetchQuote from '../redux/quotes/quoteActions';
-import '../styles/RenderUsers.css';
-import { MdOutlineEmail, MdPhoneIphone, MdOutlineKeyboardDoubleArrowDown } from "react-icons/md";
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchUsers, handleCloseRequest } from "../redux/users/usersActions";
+import fetchQuote from "../redux/quotes/quoteActions";
+import "../styles/RenderUsers.css";
+import {
+  MdOutlineEmail,
+  MdPhoneIphone,
+  MdOutlineKeyboardDoubleArrowDown,
+} from "react-icons/md";
 import { TbWorldWww } from "react-icons/tb";
 
 function RenderUsers() {
-  
-  
-  const isLoading = useSelector(state => state.user.loading);
-  const users = useSelector(state => state.user.users);
-  const quote = useSelector (state => state.quote.quote)
+  const isLoading = useSelector((state) => state.user.loading);
+  const users = useSelector((state) => state.user.users);
+  const quote = useSelector((state) => state.quote.quote);
   const dispatch = useDispatch();
 
   const handleFetchUsers = () => {
@@ -20,66 +22,77 @@ function RenderUsers() {
 
   const handleCloseButton = () => {
     dispatch(handleCloseRequest());
-
   };
 
   const handleFetchQuote = () => {
     dispatch(fetchQuote());
   };
 
-
   return (
-    <div className='homepage'>
+    <div className="homepage">
       <h1> Users </h1>
-      <p>Click the button below to see our dummy users!</p>
 
-      {(users.length === 0 ? (
-        <button onClick={()=>handleFetchUsers()} disabled={isLoading}>
-        {isLoading ? 'Loading...' : 'Click me'}
-      </button>) : <p>Select a user below to see their favourite quotes <MdOutlineKeyboardDoubleArrowDown className='down-arrow'/></p>
+      {users.length === 0 ? (
+        <div className="first-instruction">
+          <h2>Click the button below to see our dummy users!</h2>
+          <button onClick={() => handleFetchUsers()} disabled={isLoading}>
+            {isLoading ? "Loading..." : "Click me"}
+          </button>
+        </div>
+      ) : (
+        <h3>
+          Select a user below to see their favourite quotes{" "}
+          <MdOutlineKeyboardDoubleArrowDown className="down-arrow" />
+        </h3>
       )}
-      
 
-      {(users.length > 0 && quote) && (
-      <div>
-          {quote.map(quote => (
-          <div key={quote.id} className='quote-container'>
-          <p>{quote.content}</p>
-          <span className='author'>{quote.author}</span>
+      {users.length > 0 && quote && (
+        <div>
+          {quote.map((quote) => (
+            <div key={quote.id} className="quote-container shadow pulse">
+              <p>"{quote.content}"</p>
+              <span className="author"> - {quote.author}</span>
             </div>
-        ))}
+          ))}
         </div>
       )}
 
       {users && (
-        <div className='main-container'>
-        <section className='user-container'>
-          {users.map(user => (
-            <div key={user.id} className='user-card' onClick={()=>handleFetchQuote()}>
-              <div>
-                {user.name}
-                <p><MdOutlineEmail /> {user.email}</p>
-                <p><MdPhoneIphone /> {user.phone}</p>
-                <p><TbWorldWww /> {user.website}</p>
+        <div className="main-container">
+          <section className="user-container">
+            {users.map((user) => (
+              <div
+                key={user.id}
+                className="user-card"
+                onClick={() => handleFetchQuote()}
+              >
+                <div>
+                  {user.name}
+                  <p>
+                    <MdOutlineEmail /> {user.email}
+                  </p>
+                  <p>
+                    <MdPhoneIphone /> {user.phone}
+                  </p>
+                  <p>
+                    <TbWorldWww /> {user.website}
+                  </p>
+                </div>
               </div>
-              
-             
-            </div>
-          ))}
-          
+            ))}
           </section>
-          {(users.length > 0 ? (
-            <button className='close-button' onClick={() => handleCloseButton()} >Close</button>) : null
-            )}
-         
-
-          </div>
+          {users.length > 0 ? (
+            <button
+              className="close-button"
+              onClick={() => handleCloseButton()}
+            >
+              Close
+            </button>
+          ) : null}
+        </div>
       )}
-
-      
-      
-     </div>
-  )
+    </div>
+  );
 }
 
-export default RenderUsers
+export default RenderUsers;
